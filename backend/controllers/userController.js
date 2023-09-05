eab2e148637061db0afbc24f161b72e8ea598d4e
Fahim <fhasan5@wgu.edu>
2023-09-05 09:14:16 -0500

    user model & bcrypt

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,5 @@
 import asyncHandler from 'express-async-handler';
+import User from '../models/useModel.js';
 
 // @desc   Auth user/set token
 // route   POST /api/users/auth
@@ -8,4 +9,66 @@ const authUser = asyncHandler(async (req, res) => {
   res.status(200).json({ message: 'Auth User' });
 });
 
-export { authUser };
+// @desc   Register a new user
+// route   POST /api/users
+// @access Public
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error('User already exists');
+  }
+
+  //create a user
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    res.send(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid userdata');
+  }
+});
+
+// @desc   Logout user
+// route   POST /api/users/logout
+// @access Public
+
+const logoutUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: 'Logout User' });
+});
+
+// @desc   Get user profile
+// route   Get /api/users/profile
+// @access Private
+
+const getUserProfile = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: 'User profile' });
+});
+
+// @desc   Update user profile
+// route   PUT /api/users/profile
+// @access Private
+
+const UpdateUserProfile = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: 'Update User profile' });
+});
+
+export {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  UpdateUserProfile,
+};
